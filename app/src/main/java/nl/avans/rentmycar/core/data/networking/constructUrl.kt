dea54f62e8168929cd96c532e58d9be51fbed5b6
Sam Fober <36276130+SamFober@users.noreply.github.com>
2024-12-18 20:09:49 +0100

    feat: add functionality for making HTTP requests

---
+++ app/src/main/java/nl/avans/rentmycar/core/data/networking/constructUrl.kt
@@ -0,0 +1,11 @@
+package nl.avans.rentmycar.core.data.networking
+
+import nl.avans.rentmycar.BuildConfig
+
+fun constructUrl(url: String): String {
+    return when {
+        url.contains(BuildConfig.BASE_URL) -> url
+        url.startsWith("/") -> BuildConfig.BASE_URL + url.drop(1)
+        else -> BuildConfig.BASE_URL + url
+    }
+}
