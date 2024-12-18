72b2034e99a87acde6899e946d50863b75fff707
Sam Fober <36276130+SamFober@users.noreply.github.com>
2024-12-18 19:52:27 +0100

    feat: add kotlinx serialization library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.serialization.kotlinx.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
