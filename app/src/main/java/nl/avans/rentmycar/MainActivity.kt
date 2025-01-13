30d7fe8222b33f707d4e3aacc804d4c72525c07e
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-13 22:05:15 +0100

    feat: implement compose navigation

--- app/src/main/java/nl/avans/rentmycar/MainActivity.kt
+++ app/src/main/java/nl/avans/rentmycar/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
+import nl.avans.rentmycar.auth.presentation.login.LoginScreen
 import nl.avans.rentmycar.ui.theme.RentMyCarTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +21,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RentMyCarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = LoginScreen
+                ) {
+                    composable<LoginScreen> {
+                        LoginScreen()
+                    }
                 }
             }
         }
     }
 }
+
+@Serializable
+object LoginScreen
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
