56b6a637cffa6d5208a5b16d4e151c7ba6175913
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-15 19:34:35 +0100

    feat: add kotlinx datetime and android datastore libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.kotlinx.datetime)
     implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.core)
