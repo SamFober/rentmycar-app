3c42b0e9f01645ca357c0d961efd311705a4b521
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-15 11:28:45 +0100

    feat: add password visibility to ui state
    - renamed LoginState to LoginUiState

--- app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginUiState.kt
+++ app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginUiState.kt
@@ -1,7 +1,8 @@
 package nl.avans.rentmycar.auth.presentation.login
 
-data class LoginState(
+data class LoginUiState(
     val isLoading: Boolean = false,
     val emailAddress: String = "",
-    val password: String = ""
+    val password: String = "",
+    val passwordVisible: Boolean = false
 )
