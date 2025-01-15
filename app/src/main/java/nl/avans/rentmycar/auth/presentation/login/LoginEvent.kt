b30c7638fd273b274a15417a33482d3bae768f2b
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-15 16:30:49 +0100

    chore: renamed failed login event

--- app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginEvent.kt
+++ app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginEvent.kt
@@ -1,8 +1,8 @@
 package nl.avans.rentmycar.auth.presentation.login
 
-import nl.avans.rentmycar.auth.domain.LoginError
+import nl.avans.rentmycar.core.domain.util.Error
 
 sealed interface LoginEvent {
     object Success : LoginEvent
-    data class Error(val error: LoginError) : LoginEvent
+    data class Failed(val error: Error) : LoginEvent
 }
