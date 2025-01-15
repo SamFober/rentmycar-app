fe6a6731a0e29630491db03f72e68ff6a392ed59
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-15 11:31:21 +0100

    chore: remove unused login actions

--- app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginAction.kt
+++
@@ -1,7 +0,0 @@
-package nl.avans.rentmycar.auth.presentation.login
-
-sealed interface LoginAction {
-    data class EmailChanged(val email: String) : LoginAction
-    data class PasswordChanged(val password: String) : LoginAction
-    object Submit : LoginAction
-}
