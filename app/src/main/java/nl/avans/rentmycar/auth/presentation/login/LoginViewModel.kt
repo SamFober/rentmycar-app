47b12c9ec66c4a9eb8a741fe064ddd279b9ded8e
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-16 15:52:26 +0100

    chore: remove unused package imports

--- app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginViewModel.kt
+++ app/src/main/java/nl/avans/rentmycar/auth/presentation/login/LoginViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import nl.avans.rentmycar.auth.domain.AuthDataSource
