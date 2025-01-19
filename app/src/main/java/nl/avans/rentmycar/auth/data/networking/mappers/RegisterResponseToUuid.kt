4c041bb96114dae4f3ce92acec72e180155e1c6e
Sam Fober <36276130+SamFober@users.noreply.github.com>
2025-01-19 12:14:08 +0100

    chore: remove unused functions

--- app/src/main/java/nl/avans/rentmycar/auth/data/networking/mappers/RegisterResponseToUuid.kt
+++
@@ -1,8 +0,0 @@
-package nl.avans.rentmycar.auth.data.networking.mappers
-
-import nl.avans.rentmycar.auth.data.networking.dto.register.RegisterResponse
-import java.util.UUID
-
-fun RegisterResponse.toUuid(): UUID {
-    return UUID.fromString(this.userId)
-}
