565d7da29afd256c2b1ab6a55619d3a05d403469
Robin Westermann <robinwest17@gmail.com>
2025-01-25 13:21:31 +0100

    18-01-2025

--- app/src/main/java/nl/avans/rentmycar/ui/review/ReviewFeature.kt
+++ app/src/main/java/nl/avans/rentmycar/ui/review/ReviewFeature.kt
@@ -2,12 +2,9 @@ package nl.avans.rentmycar.ui.review
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -22,10 +19,10 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import nl.avans.rentmycar.ui.review.components.LoadingIndicator
 import nl.avans.rentmycar.ui.review.components.ReviewForm
 import nl.avans.rentmycar.ui.review.components.ReviewListScreen
 import nl.avans.rentmycar.ui.review.models.Review
@@ -119,14 +116,4 @@ fun ReviewFeature(
     }
 }
 
-@Composable
-fun LoadingIndicator() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        CircularProgressIndicator()
-    }
-}
+
--- app/src/main/java/nl/avans/rentmycar/ui/review/components/ReviewListScreen.kt
+++ app/src/main/java/nl/avans/rentmycar/ui/review/components/ReviewListScreen.kt
@@ -1,7 +1,6 @@
 package nl.avans.rentmycar.ui.review.components
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,11 +12,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,10 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import nl.avans.rentmycar.ui.review.models.Review
-
 
 
 @Composable
@@ -79,45 +73,6 @@ fun ReviewListScreen(
     }
 }
 
-@Composable
-fun SortDropdownMenu(
-    selectedOption: String,
-    onSortOptionChange: (String) -> Unit
-) {
-    var expanded by remember { mutableStateOf(false) }
-    val options = listOf("Newest", "Oldest", "Highest Rating")
-
-    Box(modifier = Modifier.padding(16.dp)) {
-        // Dropdown button
-        Button(onClick = { expanded = true }) {
-            Text(text = selectedOption)
-        }
-
-        // Dropdown menu
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
-            options.forEach { option ->
-                DropdownMenuItem(
-                    onClick = {
-                        onSortOptionChange(option)
-                        expanded = false
-                    },
-                    text = {
-                        Text(
-                            text = option,
-                            style = if (option == selectedOption) MaterialTheme.typography.titleMedium.copy(
-                                color = MaterialTheme.colorScheme.primary,
-                                fontWeight = FontWeight.Bold
-                            ) else MaterialTheme.typography.bodyMedium
-                        )
-                    }
-                )
-            }
-        }
-    }
-}
 
 @Composable
 fun ReviewCard(
